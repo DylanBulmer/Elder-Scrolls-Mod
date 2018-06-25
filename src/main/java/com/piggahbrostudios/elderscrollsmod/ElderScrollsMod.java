93f4e22e4b7054b5221d469351c14c96cd0d0d65
Dylan Bulmer <dylanmbulmer@gmail.com>
2018-06-25 12:55:59 -0400

    Initial Forge Setup

---
+++ src/main/java/com/piggahbrostudios/elderscrollsmod/ElderScrollsMod.java
@@ -0,0 +1,31 @@
+package com.piggahbrostudios.elderscrollsmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ElderScrollsMod.MODID, name = ElderScrollsMod.NAME, version = ElderScrollsMod.VERSION)
+public class ElderScrollsMod
+{
+    public static final String MODID = "elderscrollsmod";
+    public static final String NAME = "Elder Scrolls Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
